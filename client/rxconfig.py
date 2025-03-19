0e54429cab4f32d6fa7fd59051a091bdce2d2dd4
A.Klivtsov <Sashaklivsovpechka@gmail.com>
2025-03-19 18:25:06 +0300

    config fix

--- client/rxconfig.py
+++ client/rxconfig.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 config = rx.Config(
     app_name="client",
+    api_url=os.getenv("REFLEX_API_URL"),
     TODOAPP_SERVER_HOST=os.getenv("TODOAPP_SERVER_HOST"),
     TODOAPP_SERVER_PORT=os.getenv("TODOAPP_SERVER_PORT"),
 )
